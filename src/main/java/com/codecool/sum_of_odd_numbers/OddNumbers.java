5db2b1f54405e2b8de0f06efc9fbdb0d9add774c
Szabó György <geoedevices@live.com>
2021-03-24 15:20:03 +0100

    Finished project

--- src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
+++ src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
@@ -5,6 +5,13 @@ import java.util.List;
 public class OddNumbers {
 
     public static Integer sum(List<Integer> numbers) {
-        return null;
+        Integer integerToReturn = 0;
+        for (Integer actualNumber : numbers) {
+            if (actualNumber % 2 == 1 || actualNumber % 2 == -1) {
+                integerToReturn += actualNumber;
+            }
+        }
+        return integerToReturn;
     }
+
 }
